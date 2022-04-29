a9cfd40c4ce9d3496beb4ad3753fbd6c9a9ebdb4
Manan <manan.mehta@sts.in>
2022-04-29 19:33:19 +0530

    first commit

---
+++ orders_backend.py
@@ -0,0 +1,25 @@
+import json
+
+from kafka import KafkaProducer, producer
+
+ORDER_KAFKA_TOPIC = "order_details"
+ORDER_LIMIT = 20_000
+
+producer = KafkaProducer(bootstrap_servers="localhost:29092")
+
+print("Going to generate orders after 5 seconds")
+print("Will generate one unique order every 5 seconds")
+
+for i in range(1, ORDER_LIMIT):
+    data = {
+        "order_id": i,
+        "user_id": f'swiggy_{i}',
+        "total_cost": i*2,
+        "items": "biryani, pizza"
+    }
+
+    producer.send(
+        ORDER_KAFKA_TOPIC,
+        json.dumps(data).encode("utf-8")
+        )
+    print(f'Done Sending...{i}')
